5860096e71dfcb26845378f8fba1a1bba354f3d7
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-02-16 16:46:04 +0700

    refactor: remove unused variables/components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'antd';
 import Dashboard from './containers/layout/Dashboard';
 
 function App() {
